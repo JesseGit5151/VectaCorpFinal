add430e833506d952863f163b21909b713fb0bb4
JesseGit5151 <jrodriguez5151yahoo.com>
2021-03-14 18:23:57 -0700

    created repo

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+let {src, dest, watch} = require('gulp')
+let sass = require('gulp-sass')
+sass.compiler = require('node-sass')
+
+function css() {
+    return src('src/*.scss')
+        .pipe(sass())
+        .pipe(dest('dist/css'))
+}
+
+exports.default = function () {
+    watch('src/*.scss', css)
+    watch('src/*modules/*css', css)
+};
+//exports.default = css
